1672275b3e30d3ecf21cfa320c2e81bad9f55ce4
Pierre <118437480+SocksThatRock@users.noreply.github.com>
2023-05-08 12:15:38 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-var coll = document.getElementsByClassName("collapsible");
+var acc = document.getElementsByClassName("accordion");
 var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    } 
-  });
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(a){
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    }
+}
+var sub_acc = document.getElementsByClassName("sub_accordion");
+var i2;
+
+for (i2 = 0; i2 < sub_acc.length; i2++) {
+    sub_acc[i2].onclick = function(a){
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    }
 }
